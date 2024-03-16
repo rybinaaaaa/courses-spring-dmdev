45e95ddd01854cdaaa3c9fe17789bb2a150068c9
rybinali <rybinaaaaa.a@gmail.com>
2024-03-16 19:09:07 +0100

    bean with not def constructor

--- src/main/java/core/rybina/ApplicationRunner.java
+++ src/main/java/core/rybina/ApplicationRunner.java
@@ -8,7 +8,7 @@ public class ApplicationRunner {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 //        Map: key(адрес класса + # + айди): Object
         System.out.println(context.getBean(ConnectionPool.class)); // если бинов одного класса несклоько, то мы не можем доставать бин по классу (непонятно какой из нескольких брать)
-        System.out.println(context.getBean("id2",  ConnectionPool.class));
+//        System.out.println(context.getBean("id2",  ConnectionPool.class));
         System.out.println(context.getBean("core.rybina.database.ConnectionPool#0",  ConnectionPool.class));
     }
 }
