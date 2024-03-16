a95e61a4fa4a4e044ffabc07904f8a233f71b0d5
rybinali <rybinaaaaa.a@gmail.com>
2024-03-16 18:56:18 +0100

    bean definition, add names and id

--- src/main/java/core/rybina/ApplicationRunner.java
+++ src/main/java/core/rybina/ApplicationRunner.java
@@ -6,6 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        System.out.println(context.getBean(ConnectionPool.class));
+//        Map: key(адрес класса + # + айди): Object
+        System.out.println(context.getBean(ConnectionPool.class)); // если бинов одного класса несклоько, то мы не можем доставать бин по классу (непонятно какой из нескольких брать)
+        System.out.println(context.getBean("id2",  ConnectionPool.class));
+        System.out.println(context.getBean("core.rybina.database.ConnectionPool#0",  ConnectionPool.class));
     }
 }
