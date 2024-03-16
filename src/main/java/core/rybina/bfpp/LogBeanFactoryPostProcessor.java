8533c87d60a1400aff1562046b324e439d123b56
rybinali <rybinaaaaa.a@gmail.com>
2024-03-16 22:15:28 +0100

    add own postprocessor

---
+++ src/main/java/core/rybina/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,19 @@
+package core.rybina.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, Ordered {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
---
+++ src/main/java/core/rybina/bfpp/VerifyPropertyLogBeanFactoryPostProcessor.java
@@ -0,0 +1,19 @@
+package core.rybina.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+
+public class VerifyPropertyLogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
