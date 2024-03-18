01b0bc31bad3bd131ccd89ae19f5176c37d7560d
rybinali <rybinaaaaa.a@gmail.com>
2024-03-18 18:20:45 +0100

    create own @Autowired annotation

--- src/main/java/core/rybina/database/pool/CompanyRepository.java
+++ src/main/java/core/rybina/database/pool/CompanyRepository.java
@@ -1,16 +1,10 @@
 package core.rybina.database.pool;
 
+import core.rybina.beanPostProcessor.InjectBean;
 import core.rybina.database.ConnectionPool;
 
 public class CompanyRepository {
 
-    private final ConnectionPool connectionPool;
-
-    private CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
