a6827c0e9c159e5d4355c50284aefc1fe9cf7929
rybinali <rybinaaaaa.a@gmail.com>
2024-03-18 17:28:12 +0100

    add second postprocessor

--- src/main/java/core/rybina/bfpp/VerifyPropertyLogBeanFactoryPostProcessor.java
+++ src/main/java/core/rybina/bfpp/VerifyPropertyLogBeanFactoryPostProcessor.java
@@ -3,9 +3,10 @@ package core.rybina.bfpp;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.core.Ordered;
 import org.springframework.core.PriorityOrdered;
 
+
+//interface PriorityOrdered >> any value of interface Ordered
 public class VerifyPropertyLogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
