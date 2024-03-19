aa72b409688a9823863c2aa4faf09414a5bf9c9e
rybinali <rybinaaaaa.a@gmail.com>
2024-03-19 17:11:00 +0100

    add @Profiles -v1

--- src/main/java/core/rybina/config/ConfigurationApplication.java
+++ src/main/java/core/rybina/config/ConfigurationApplication.java
@@ -1,17 +1,20 @@
 package core.rybina.config;
 
 import core.rybina.database.ConnectionPool;
+import core.rybina.database.pool.CompanyRepository;
 import core.rybina.database.pool.CrudRepository;
 import core.web.config.ConfigurationWebApplication;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 import static org.springframework.context.annotation.ComponentScan.*;
 
 //@ImportResource("classpath:application.xml")
-@Import(ConfigurationWebApplication.class)
 @Configuration
+@Import(ConfigurationWebApplication.class)
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "core.rybina", useDefaultFilters = false, includeFilters = {
         @Filter(type = FilterType.ANNOTATION, value = Component.class),
@@ -23,5 +26,16 @@ public class ConfigurationApplication {
     @Bean
     public ConnectionPool pool2(@Value("${db.username}") String username) {
         return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    @Profile("!prod")
+    public ConnectionPool pool3(){
+        return new ConnectionPool("test-pool", 25);
+    }
+
+    @Bean
+    public CompanyRepository companyRepository3() {
+        return new CompanyRepository(pool3(), List.of(pool3()));
     }
 }
--- src/main/java/core/web/config/ConfigurationWebApplication.java
+++ src/main/java/core/web/config/ConfigurationWebApplication.java
@@ -1,4 +1,10 @@
 package core.web.config;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!web&&prod")
+@Configuration()
 public class ConfigurationWebApplication {
 }
