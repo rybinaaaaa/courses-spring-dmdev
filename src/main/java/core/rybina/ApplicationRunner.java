4f0cc19b8da65dcf12f012b9768665d431cab1fd
rybinali <rybinaaaaa.a@gmail.com>
2024-03-24 20:10:34 +0100

    add springboot

--- src/main/java/core/rybina/ApplicationRunner.java
+++ src/main/java/core/rybina/ApplicationRunner.java
@@ -1,17 +1,12 @@
 package core.rybina;
 
-import core.rybina.config.ConfigurationApplication;
-import core.rybina.database.service.CompanyService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApplicationRunner {
+
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationApplication.class)) {
-            CompanyService companyService = context.getBean(CompanyService.class);
-//            context.getEnvironment().setActiveProfiles("web", "prod");
-////            ПЕРЕСОЗДАЕМ БИНЫ, ЧТОБЫ НЕПОДХОДЯЩИЕ НЕ СОЗДАВАЛИСЬ!
-//            context.refresh();
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
