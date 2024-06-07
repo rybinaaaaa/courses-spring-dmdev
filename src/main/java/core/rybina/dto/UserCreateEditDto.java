e9be1a74becaf3b84b7bd240060ae893642c9843
rybinali <rybinaaaaa.a@gmail.com>
2024-06-07 14:08:02 +0200

    add validation - 1

--- src/main/java/core/rybina/dto/UserCreateEditDto.java
+++ src/main/java/core/rybina/dto/UserCreateEditDto.java
@@ -1,6 +1,9 @@
 package core.rybina.dto;
 
 import core.rybina.database.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Value;
 import lombok.experimental.FieldNameConstants;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -10,11 +13,21 @@ import java.time.LocalDate;
 @FieldNameConstants
 @Value
 public class UserCreateEditDto {
+
+    @Email
     String username;
+
+    @NotNull
     String firstname;
+
+    @NotNull
+    @Size(min = 3, max = 50)
     String lastname;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+
+    @NotNull
     LocalDate birthdate;
+
     Role role;
+
     Integer companyId;
 }
--- src/main/java/core/rybina/http/controller/UserController.java
+++ src/main/java/core/rybina/http/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -48,17 +50,18 @@ public class UserController {
 
     @PostMapping
 //    @ResponseStatus(HttpStatus.CREATED)
-    public String create(@ModelAttribute UserCreateEditDto user, RedirectAttributes redirectAttributes) {
-//        if (true) {
-//            redirectAttributes.addFlashAttribute("user", user);
-//            return "redirect:/users/registration";
-//        }
+    public String create(@ModelAttribute @Validated UserCreateEditDto user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("user", user);
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getFieldErrors());
+            return "redirect:/users/registration";
+        }
         return "redirect:/users/" + userService.create(user).getId();
     }
 
     //    @PutMapping("/{id}")
     @PostMapping("/{id}/update")
-    public String update(@PathVariable("id") Long id, @ModelAttribute UserCreateEditDto user) {
+    public String update(@PathVariable("id") Long id, @ModelAttribute @Validated UserCreateEditDto user) {
         return userService.update(id, user)
                 .map(it -> "redirect:/users/{id}")
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
