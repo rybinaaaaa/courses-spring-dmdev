042364505ac17d520f72e75003fa4853f6f5b89a
rybinali <rybinaaaaa.a@gmail.com>
2024-06-13 14:51:52 +0200

    add using locales to thymeleaf

--- src/main/java/core/rybina/config/LocaleConfiguration.java
+++ src/main/java/core/rybina/config/LocaleConfiguration.java
@@ -4,15 +4,26 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
-public class MessageSourceConfiguration {
+public class LocaleConfiguration {
 
     @Bean
     public MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("locales/messages"); 
+        messageSource.setBasename("locales/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
 }
--- src/main/java/core/rybina/config/WebConfigurer.java
+++ src/main/java/core/rybina/config/WebConfigurer.java
@@ -1,9 +1,12 @@
 package core.rybina.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
@@ -11,5 +14,17 @@ public class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(Jsr310Converters.StringToLocalDateConverter.INSTANCE);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
     }
 }
