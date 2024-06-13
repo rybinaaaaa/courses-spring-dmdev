262fd11d2170db99a1334d8280ab39bc5eeee651
rybinali <rybinaaaaa.a@gmail.com>
2024-06-13 14:31:16 +0200

    add usage locales

---
+++ src/main/java/core/rybina/http/rest/MessageRestController.java
@@ -0,0 +1,23 @@
+package core.rybina.http.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping("/api/v1/messages")
+@RequiredArgsConstructor
+public class MessageRestController {
+
+    private final MessageSource messageSource;
+
+    @GetMapping
+    public String getMessage(@RequestParam("key") String key, @RequestParam("language") String language) {
+        return messageSource.getMessage(key, null, null, Locale.of(language));
+    }
+}
